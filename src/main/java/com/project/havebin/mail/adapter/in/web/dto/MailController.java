3ceb8e4bdf55781069855dad5783dacc8f85cab5
HYEONSIK <ks06891@naver.com>
2025-10-26 20:26:44 +0900

    feat: update API URL
    - API URL 구조를 수정
    - API 설명을 Swagger에 추가

--- src/main/java/com/project/havebin/mail/adapter/in/web/dto/MailController.java
+++ src/main/java/com/project/havebin/mail/adapter/in/web/dto/MailController.java
@@ -3,30 +3,31 @@ package com.project.havebin.mail.adapter.in.web.dto;
 import com.project.havebin.mail.adapter.in.web.dto.request.DuplicateMailReqDto;
 import com.project.havebin.mail.adapter.in.web.dto.request.EmailAuthReqDto;
 import com.project.havebin.mail.application.port.in.MailUseCase;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
 @RestController
 @AllArgsConstructor
+@RequestMapping("/api/mail")
 @Tag(name = "Mail API", description = "메일 전송 및 인증 관련 API")
 public class MailController {
     private final MailUseCase mailUseCase;
 
-    @PostMapping("/validateDuplicateUser")
+    @PostMapping("/duplicateMail")
+    @Operation(summary = "메일 중복 조회 및 인증번호 전송", description = "이메일 중복 확인 & 존재하지 않는다면 인증번호 전송.")
     public ResponseEntity<?> duplicateMail(@Valid @RequestBody DuplicateMailReqDto dto) {
         mailUseCase.duplicateMail(dto.toCommand());
         return ResponseEntity.ok("success");
     }
 
-    @PostMapping("/mailAuth")
+    @PostMapping("/auth")
+    @Operation(summary = "이메일 인증번호 확인")
     public ResponseEntity<?> mailCodeAuth(@Valid @RequestBody EmailAuthReqDto dto) {
         mailUseCase.emailAuth(dto.toCommand());
         return ResponseEntity.ok("success");
--- src/main/java/com/project/havebin/user/adapter/in/web/UserController.java
+++ src/main/java/com/project/havebin/user/adapter/in/web/UserController.java
@@ -15,30 +15,31 @@ import java.util.Map;
 
 @RestController
 @AllArgsConstructor
+@RequestMapping("/api/user")
 @Tag(name = "User API", description = "유저 관련 API")
 public class UserController {
 
     private final UserUseCase userUseCase;
 
-    @PostMapping("/responseUserInfo")
+    @PostMapping("/join")
     @Operation(summary = "회원가입")
     public ResponseEntity<?> createUser(@Valid @RequestBody UserSignReqDto reqDto) {
         return ResponseEntity.ok(userUseCase.createUser(reqDto.toCommand()));
     }
 
-    @PostMapping("/validateDuplicateNickname")
+    @PostMapping("/duplicateNickname")
     @Operation(summary = "닉네임 중복검사")
     public ResponseEntity<?> duplicateNickname(@Valid @RequestBody DuplicateNicknameReqDto reqDto) {
         return ResponseEntity.ok(userUseCase.duplicateNickname(reqDto.toCommand()));
     }
 
-    @DeleteMapping("/deleteUser")
+    @DeleteMapping("/")
     @Operation(summary = "회원탈퇴")
     public ResponseEntity<?> deleteUser(/*@AuthenticationPrincipal Long id*/) {
         return ResponseEntity.ok("Not Implemented");
     }
 
-    @GetMapping("/getUserdata")
+    @GetMapping("/")
     @Operation(summary = "유저 정보 조회", description = "(로그인 기능 미완성) 임시로 1번 유저만 조회")
     public ResponseEntity<?> getUserData(/*@AuthenticationPrincipal Long id*/) {
         Long id = 1L; // 로그인 기능 미완성
